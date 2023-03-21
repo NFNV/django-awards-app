7227ef994d2bbbe50d2b73813a648152dad3afe0
Nicolás Valiconto <nvaliconto@outlook.com>
2023-03-21 11:51:18 -0300

    templates

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse(f'You are on the main page')
+
+def detail(request, question_id):
+    return HttpResponse(f'You are reading question {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'You are reading the results of question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting to the question {question_id}')
