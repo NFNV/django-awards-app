f17b0df586c5a84bcf50204095fdca87343b8bab
Nicolás Valiconto <nvaliconto@outlook.com>
2023-03-21 16:20:32 -0300

    404

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+from .models import Question
 
 def index(request):
-    return HttpResponse(f'You are on the main page')
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
 
 def detail(request, question_id):
-    return HttpResponse(f'You are reading question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
 
 def results(request, question_id):
     return HttpResponse(f'You are reading the results of question {question_id}')
