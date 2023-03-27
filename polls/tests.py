57acf94905cb265e8ad01d272ce12cfe58023545
Nicolás Valiconto <nvaliconto@outlook.com>
2023-03-27 17:04:00 -0300

    question model test fix

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,7 @@ from .models import Question
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_questions(self):
-    # was_punlished_recently returns False for questions whose pub_date is in the future
-    time = timezone.now() + datetime.timedelta(days=30)
-    future_question = Question(question_text='Which is the best screenplay?', pub_date=time)
-    self.assertIs(future_question.was_published_recently, False)
+        # was_punlished_recently returns False for questions whose pub_date is in the future
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text='Which is the best screenplay?', pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
