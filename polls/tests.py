f3c0217f3b386428aa2aad81850936b0275d8d5d
Nicolás Valiconto <nvaliconto@outlook.com>
2023-03-29 17:23:13 -0300

    question model test v2

--- polls/tests.py
+++ polls/tests.py
@@ -10,3 +10,15 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text='Which is the best screenplay?', pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_questions(self):
+        # was_published_recently() must return Flase for questions whose pub_date is more than 1 day in the past
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text='Which is the best screenplay?',pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        # was_published_recently() must return True for questions whose pub_date is actual
+        time = timezone.now()
+        present_question = Question(question_text='Which is the best screenplay?',pub_date=time)
+        self.assertIs(present_question.was_published_recently(), True)
